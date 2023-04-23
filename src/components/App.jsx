10fb67a543c816f5fe3da4d0fd55f3c8886163c3
Dominik Lenart <dominik.lenart84@gmail.com>
2023-04-23 20:29:29 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { useEffect } from 'react';
+import { ContactList } from './ContactList/ContactList';
+import { AddContact } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+import { useSelector } from 'react-redux';
+import { loadContacts } from './../redux/selectors';
+
 export const App = () => {
+  const contacts = useSelector(loadContacts);
+
+  useEffect(() => {
+    if (contacts.length > 0) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1> Phonebook</h1>
+      <AddContact />
+
+      <h2> Contacts </h2>
+      {contacts.length > 1 && <Filter />}
+      {contacts.length > 0 && <ContactList />}
+      {contacts.length === 0 && <p>Your contact list is empty</p>}
     </div>
   );
 };
